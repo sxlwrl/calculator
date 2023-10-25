aa0bc7aadb0e03448a780389045877a0f8867edf
Vadym <vadymkazak2019@gmail.com>
2023-10-26 00:26:57 +0300

    some division changes

--- calculator.h
+++ calculator.h
@@ -7,6 +7,15 @@ class Calculator
         int Add (int, int);
         int Sub (double, double);
         int Mul (double, double);
+        int Div (double, double);
 };
+
+/**
+
+Division field has been added
+
+Have a nice day!
+
+/**
 
 #endif//CALCULATOR_H
